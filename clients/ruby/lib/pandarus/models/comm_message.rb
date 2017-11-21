39dc01c4c6d832cefeb195d3c1134559f754a9ec
Simon Williams <simon@instructure.com>
2017-11-21 11:02:54 -0700

    bring comm_message current
    Change-Id: I3085676d4440b115399399d2ac95254552ff9420

--- clients/ruby/lib/pandarus/models/comm_message.rb
+++ clients/ruby/lib/pandarus/models/comm_message.rb
@@ -10,6 +10,7 @@ module Pandarus
     attribute :sent_at, resolve_type("DateTime")
     attribute :workflow_state, resolve_type("String")
     attribute :from, resolve_type("String")
+    attribute :from_name, resolve_type("String")
     attribute :to, resolve_type("String")
     attribute :reply_to, resolve_type("String")
     attribute :subject, resolve_type("String")
